9021eb351dca662cd6ab00d331e8bc73c70eb89f
Matthew Parker <maddparker@gmail.com>
2020-02-03 16:53:55 -0500

    Commented the code

--- Week1-CodeLab0-Refresher/Assets/Scripts/PlayerController.cs
+++ Week1-CodeLab0-Refresher/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float force = 5;
+    public float force = 5; //public var for force amount
 
-    Rigidbody2D rb;
+    Rigidbody2D rb; //var for the Rigidbody2D
 
     // Start is called before the first frame update
     void Start() //setup
     {
-        Debug.Log("Hello, World!");
+        Debug.Log("Hello, World!"); //print "Hello, World!" to console
 
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //get the Rigidbody2D  off of this gameObject
 
         //rb.AddForce(Vector2.right * force);
     }
@@ -21,23 +21,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() //draw
     {
-        Debug.Log("Goodbye, World!");
+        Debug.Log("Goodbye, World!"); //print "Goodbye, World!" to console
 
-        if(Input.GetKey(KeyCode.W)){
-            rb.AddForce(Vector2.up * force);
+        if (Input.GetKey(KeyCode.W))//if W is pressed
+        {
+            rb.AddForce(Vector2.up * force); //apply to the up mult by the "force" var
         }
 
-        if(Input.GetKey(KeyCode.S)){
-            rb.AddForce(Vector2.down * force);
+        if(Input.GetKey(KeyCode.S))//if S is pressed
+        {
+            rb.AddForce(Vector2.down * force); //apply to the down mult by the "force" var
         }
 
-        if(Input.GetKey(KeyCode.A)){
-            rb.AddForce(Vector2.left * force);
+        if(Input.GetKey(KeyCode.A))//if A is pressed
+        { 
+            rb.AddForce(Vector2.left * force); //apply to the left mult by the "force" var
         }
 
         if (Input.GetKey(KeyCode.D)) //if D is pressed
         {
-            rb.AddForce(Vector2.right * force); //apply a force using the "force" var
+            rb.AddForce(Vector2.right * force); //apply to the right mult by the "force" var
         }
     }
 }
--- Week1-CodeLab0-Refresher/Assets/Scripts/PrizeScript.cs
+++ Week1-CodeLab0-Refresher/Assets/Scripts/PrizeScript.cs
@@ -16,8 +16,8 @@ public class PrizeScript : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
     {
-        transform.position = new Vector2(Random.Range(-6, 6), Random.Range(-4, 4));
+        transform.position = new Vector2(Random.Range(-6, 6), Random.Range(-4, 4)); //teleport to a random location
     }
 }
