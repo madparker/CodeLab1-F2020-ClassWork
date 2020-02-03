7bc020272a7de6959f1acf2e33ba340af3cf8d4e
Matthew Parker <maddparker@gmail.com>
2020-02-03 15:44:38 -0500

    Added the new files

---
+++ Week1-CodeLab0-Refresher/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float force = 5;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() //setup
+    {
+        Debug.Log("Hello, World!");
+
+        rb = GetComponent<Rigidbody2D>();
+
+        //rb.AddForce(Vector2.right * force);
+    }
+
+    // Update is called once per frame
+    void Update() //draw
+    {
+        Debug.Log("Goodbye, World!");
+
+        if(Input.GetKey(KeyCode.W)){
+            rb.AddForce(Vector2.up * force);
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            rb.AddForce(Vector2.down * force);
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            rb.AddForce(Vector2.left * force);
+        }
+
+        if (Input.GetKey(KeyCode.D)) //if D is pressed
+        {
+            rb.AddForce(Vector2.right * force); //apply a force using the "force" var
+        }
+    }
+}
---
+++ Week1-CodeLab0-Refresher/Assets/Scripts/PrizeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrizeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.position = new Vector2(Random.Range(-6, 6), Random.Range(-4, 4));
+    }
+}
