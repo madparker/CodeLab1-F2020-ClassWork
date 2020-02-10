50e34f87776f6080d7b139616fa30f0597b4de47
Matthew Parker <maddparker@gmail.com>
2020-02-10 14:21:40 -0500

    Added Week 2 Project - Singletons

---
+++ Week2-CodeLab1-Singleton/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float force = 5; //public var for force amount
+
+    public int score = 0;
+
+    Rigidbody2D rb; //var for the Rigidbody2D
+
+    public static PlayerController instance;
+
+    private void Awake()
+    {
+        if (instance == null) //instance hasn't been set yet
+        {
+            instance = this; //set instance to this object
+            DontDestroyOnLoad(gameObject); //Dont Destroy this object when you load a new scene
+        } else { //if the instance is already set to an object
+            Destroy(gameObject); //destroy this new object, so there is only ever one
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() //setup
+    {
+        Debug.Log("Hello, World!"); //print "Hello, World!" to console
+
+        rb = GetComponent<Rigidbody2D>(); //get the Rigidbody2D  off of this gameObject
+
+        //rb.AddForce(Vector2.right * force);
+    }
+
+    // Update is called once per frame
+    void Update() //draw
+    {
+        Debug.Log("Goodbye, World!"); //print "Goodbye, World!" to console
+
+        if (Input.GetKey(KeyCode.W))//if W is pressed
+        {
+            rb.AddForce(Vector2.up * force); //apply to the up mult by the "force" var
+        }
+
+        if(Input.GetKey(KeyCode.S))//if S is pressed
+        {
+            rb.AddForce(Vector2.down * force); //apply to the down mult by the "force" var
+        }
+
+        if(Input.GetKey(KeyCode.A))//if A is pressed
+        { 
+            rb.AddForce(Vector2.left * force); //apply to the left mult by the "force" var
+        }
+
+        if (Input.GetKey(KeyCode.D)) //if D is pressed
+        {
+            rb.AddForce(Vector2.right * force); //apply to the right mult by the "force" var
+        }
+    }
+}
---
+++ Week2-CodeLab1-Singleton/Assets/Scripts/PrizeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrizeScript : MonoBehaviour
+{
+    public static int currentLevel = 0;
+    public int targetScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetScore = PlayerController.instance.score * 2 + 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
+    {
+        PlayerController.instance.score++;
+        Debug.Log("Score: " + PlayerController.instance.score);
+        transform.position = new Vector2(Random.Range(-6, 6), Random.Range(-4, 4)); //teleport to a random location
+
+        if(PlayerController.instance.score > targetScore)
+        {
+            currentLevel++;
+            SceneManager.LoadScene(currentLevel);
+        }
+    }
+}
