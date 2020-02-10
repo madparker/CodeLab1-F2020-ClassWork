a96c86e227269c5d42901ee266c594ac3bdbcae8
Matthew Parker <maddparker@gmail.com>
2020-02-10 15:31:40 -0500

    Added Time and UI for Scores to GameManager

--- Week2-CodeLab1-Singleton/Assets/Scripts/GameManager.cs
+++ Week2-CodeLab1-Singleton/Assets/Scripts/GameManager.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public Text infoText;
+
+    private float timer = 0;
+
+    private void Awake()
+    {
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        infoText = GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        infoText.text = "Score: " + PlayerController.instance.score + " Time: " + (int)timer;
     }
 }
