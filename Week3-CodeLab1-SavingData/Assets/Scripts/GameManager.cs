7e8f10b5ab39bb0b66edba2f9f33edb867e6b5f7
Matthew Parker <maddparker@gmail.com>
2020-02-24 13:43:29 -0500

    Added Week 3 Project moved score to GameManger

---
+++ Week3-CodeLab1-SavingData/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //static variable means the value is the same for all the objects of this class type and the class itself
+    public static GameManager instance; //this static var will hold the Singleton
+
+    public Text infoText; //Text Component to tell you the time and the score
+
+    private float timer = 0; //keep track of time
+
+    public int score = 0;
+
+    private void Awake()
+    {
+        if(instance == null){ //instance hasn't been set yet
+            instance = this; //set instance to this object
+            DontDestroyOnLoad(gameObject); //Dont Destroy this object when you load a new scene
+        } else { //if the instance is already set to an object
+            Destroy(gameObject); //destroy this new object, so there is only ever one
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        infoText = GetComponentInChildren<Text>(); //get the text component from the children of this gameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; //increase the timer by deltaTime every frame
+        infoText.text = "Score: " + GameManager.instance.score + " Time: " + (int)timer; //update the text component with the score and time
+    }
+}
