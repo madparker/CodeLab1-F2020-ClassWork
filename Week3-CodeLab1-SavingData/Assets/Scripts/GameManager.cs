8ca88344f4c9f9ecbc53467d6b38e12393750772
Matthew Parker <maddparker@gmail.com>
2020-02-28 14:41:20 -0500

    Commented Code

--- Week3-CodeLab1-SavingData/Assets/Scripts/GameManager.cs
+++ Week3-CodeLab1-SavingData/Assets/Scripts/GameManager.cs
@@ -13,29 +13,32 @@ public class GameManager : MonoBehaviour
 
     private float timer = 0; //keep track of time
 
+    private const string PLAY_PREF_KEY_HS = "High Score"; //Player preferences key for high score. It is a "const" which means the value is only set once and can never be changed
+  
+    private const string FILE_HS = "/CodeLab1-S2020-highscore.txt"; //Const for name of the highscore file
+    private const string FILE_ALL_SCORES = "/All_scores.txt"; //Const for the name of the All Scores file
+
     private int score = 0;
-
-    private const string PLAY_PREF_KEY_HS = "High Score";
-    private const string PLAY_PREF_KEY_P1_HS = "P1 HIGH SCORE";
-
-    private const string FILE_HS = "/CodeLab1-S2020-highscore.txt";
-    private const string FILE_ALL_SCORES = "/All_scores.txt";
-
-    //Property
+    //Property for score
+    //A property wraps a variable and allows you to call a function
+    //whenever the value of the variable is used or set
     public int Score{
         get{
             return score;
         }
         set{
-            score = value;
-            if(score > highScore){
-                HighScore = score;
+            score = value; //set "score" to whatever value was passed
+            if(score > highScore){ //if the score is greater than the highScore
+                HighScore = score; //set the HighScore property to score
             }
         }
     }
 
+    private List<int> allScores = new List<int>(); //list to hold all high scores
+
     private int highScore = 0;
 
+    //Property for HighScore
     private int HighScore{
         get{
             return highScore;
@@ -43,21 +46,21 @@ public class GameManager : MonoBehaviour
         set{
             highScore = value;
             //Save it somewhere
-            //PlayerPrefs.SetInt(PLAY_PREF_KEY_HS, highScore);
+            //PlayerPrefs.SetInt(PLAY_PREF_KEY_HS, highScore); //save HighScore to PlayerPrefs
+
+            //Save High Score to a file
             File.WriteAllText(Application.dataPath + FILE_HS, highScore + "");
         
-
+            //Add the high Score to the allScores list
             allScores.Add(highScore);
 
-            string allScoreString = "";
-            for (int i = 0; i < allScores.Count; i++){
-                allScoreString = allScoreString + allScores[i] + ",";
+            string allScoreString = ""; //make an empty string
+            for (int i = 0; i < allScores.Count; i++){ //loop through from 0 to the number of items in allScores
+                allScoreString = allScoreString + allScores[i] + ","; //add the high score in the ith place to the string followed by a ","
             }
-            File.WriteAllText(Application.dataPath + FILE_ALL_SCORES, allScoreString);
+            File.WriteAllText(Application.dataPath + FILE_ALL_SCORES, allScoreString); //write the string to the all scores file
         }
     }
-
-    private List<int> allScores = new List<int>();
 
     private void Awake()
     {
@@ -75,17 +78,19 @@ public class GameManager : MonoBehaviour
         Debug.Log(Application.dataPath);
 
         infoText = GetComponentInChildren<Text>(); //get the text component from the children of this gameObject
-        //highScore = PlayerPrefs.GetInt(PLAY_PREF_KEY_HS, 5);
-        if (File.Exists(Application.dataPath + FILE_HS))
+
+        //highScore = PlayerPrefs.GetInt(PLAY_PREF_KEY_HS, 5); //get the high score out of PlayerPrefs
+
+        if (File.Exists(Application.dataPath + FILE_HS)) //if the file exists
         {
-            string hsString = File.ReadAllText(Application.dataPath + FILE_HS);
+            string hsString = File.ReadAllText(Application.dataPath + FILE_HS); //read the text from the file into a string
 
-            print(hsString);
-            string[] splitString = hsString.Split(',');
-            highScore = int.Parse(splitString[0]);
+            print(hsString); //print the contents of the file
+            string[] splitString = hsString.Split(','); //split the string on ','
+            highScore = int.Parse(splitString[0]); //parse the string in the first place
 
-            for (int i = 0; i < splitString.Length; i++){
-                print(splitString[i]);
+            for (int i = 0; i < splitString.Length; i++){ //go through the split string array
+                print(splitString[i]); //print out each value
             }
         }
     }   
