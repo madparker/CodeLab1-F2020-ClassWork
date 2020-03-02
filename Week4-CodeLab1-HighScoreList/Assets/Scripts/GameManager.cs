fb490f737f5cd6f7ac7cb664d0a68893d05e8667
Matthew Parker <maddparker@gmail.com>
2020-03-02 12:47:37 -0500

    Added Week 4 basic template

---
+++ Week4-CodeLab1-HighScoreList/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //static variable means the value is the same for all the objects of this class type and the class itself
+    public static GameManager instance; //this static var will hold the Singleton
+
+    public Text infoText; //Text Component to tell you the time and the score
+
+    private float timer = 0; //keep track of time
+
+    private int score = 0;
+    //Property for score
+    //A property wraps a variable and allows you to call a function
+    //whenever the value of the variable is used or set
+    public int Score{
+        get{
+            return score;
+        }
+        set{
+            score = value; //set "score" to whatever value was passed
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null){ //instance hasn't been set yet
+            instance = this; //set instance to this object
+            DontDestroyOnLoad(gameObject); //Dont Destroy this object when you load a new scene
+        } else { //if the instance is already set to an object
+            Destroy(gameObject); //destroy this new object, so there is only ever one
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Application.dataPath);
+
+        infoText = GetComponentInChildren<Text>(); //get the text component from the children of this gameObject
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; //increase the timer by deltaTime every frame
+        infoText.text = "Score: " + GameManager.instance.score + " Time: " + (int)timer; //update the text component with the score and time
+    }
+}
---
+++ Week4-CodeLab1-HighScoreList/Assets/Scripts/PrizeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrizeScript : MonoBehaviour
+{
+    public static int currentLevel = 0;
+    public int targetScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetScore = GameManager.instance.Score * 2 + 0; //increase the target score every level
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
+    {
+        GameManager.instance.Score++; //increase the player's score using the Singleton!
+        Debug.Log("Score: " + GameManager.instance.Score); //print the score to console, using the Singleton
+        transform.position = new Vector2(Random.Range(-6, 6), Random.Range(-4, 4)); //teleport to a random location
+
+        if(GameManager.instance.Score > targetScore) //if the current score >  the targetScore
+        {
+            currentLevel++; //increate the level number
+            SceneManager.LoadScene(currentLevel); //go to the next level
+        }
+    }
+}
